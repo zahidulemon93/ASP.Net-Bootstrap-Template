abeada89e24c2bacfd64dd03b3c7853a62ab0f7b
User-22 <user-22@iot.datasoft.com>
2018-01-18 15:28:43 +0600

    First commit

---
+++ bootstrapTemplate/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bootstrapTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult aboutUs()
+        {
+            return View();
+        }
+        public ActionResult blog()
+        {
+            return View();
+
+        }
+
+        public ActionResult contactUs()
+        {
+            return View();
+        }
+
+        public ActionResult portfolio()
+        {
+            return View();
+        }
+        public ActionResult services()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ bootstrapTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bootstrapTemplate.Startup))]
+namespace bootstrapTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
